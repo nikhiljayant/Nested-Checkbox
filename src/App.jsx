3ee97881693d7c62cb6df10075e627df52741ca5
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-04-13 22:28:59 +0530

    add nested data structure for fruit categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,52 @@
 import React from "react"
 
+const data = [
+  {
+    id: 1,
+    name: "Mango",
+    children: [
+      {
+        id: 2,
+        name: "Banana",
+      },
+      {
+        id: 3,
+        name: "Apple",
+        children: [
+          {
+            id: 4,
+            name: "Orange",
+          },
+          {
+            id: 5,
+            name: "Grape",
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 6,
+    name: "Pineapple",
+    children: [
+      {
+        id: 7,
+        name: "Blueberry",
+      },
+      {
+        id: 8,
+        name: "Strawberry",
+        children: [
+          {
+            id: 9,
+            name: "Raspberry",
+          }
+        ]
+      }
+    ]
+  }
+]
+
 function App() {
 
   return (
