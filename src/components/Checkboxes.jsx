f86d3411e71340c85bd2b456318e7d93139981b9
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-05-10 23:10:36 +0530

    Functionality: If Parent is checked then all of it's corresponding children's should also get checked

--- src/components/Checkboxes.jsx
+++ src/components/Checkboxes.jsx
@@ -1,29 +1,44 @@
 import React from "react";
 
 const Checkboxes = ({ data = [], checked = {}, setChecked = () => {} }) => {
-  const handleChange = (e, id) => {
+  const handleChange = (isChecked, node) => {
     setChecked((prev) => {
-      return { ...prev, [id]: e.target.checked };
+      const updatedState = { ...prev, [node?.id]: isChecked };
+
+      // If Parent is checked then all the corresponding childrens should also get checked
+      const handleChildrens = (node) => {
+        if (node?.children) {
+          node?.children?.forEach((child) => {
+            updatedState[child?.id] = isChecked;
+            if (child?.children) {
+              handleChildrens(child);
+            }
+          });
+        }
+      };
+      handleChildrens(node);
+
+      return updatedState;
     });
   };
 
   return (
     <div className="flex flex-col gap-[10px]">
       {data?.length > 0 &&
-        data?.map((item) => {
+        data?.map((node) => {
           return (
-            <div key={item?.id} className="flex flex-col gap-[10px] pl-[10px]">
+            <div key={node?.id} className="flex flex-col gap-[10px] pl-[10px]">
               <div className="flex items-center gap-[10px]">
                 <input
                   type="checkbox"
-                  checked={checked[item?.id]}
-                  onChange={(e) => handleChange(e, item?.id)}
+                  checked={checked[node?.id]}
+                  onChange={(e) => handleChange(e.target.checked, node)}
                 />
-                <p>{item?.name}</p>
+                <p>{node?.name}</p>
               </div>
-              {item?.children && item?.children?.length > 0 && (
+              {node?.children && node?.children?.length > 0 && (
                 <Checkboxes
-                  data={item?.children}
+                  data={node?.children}
                   checked={checked}
                   setChecked={setChecked}
                 />
